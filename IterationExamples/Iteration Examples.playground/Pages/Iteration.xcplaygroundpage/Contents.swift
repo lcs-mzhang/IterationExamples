bdeb979cfd96fddcaa0b37ae3c5abfc17e202ef3
Zhang Matt <mzhang@lcs-mba3155.local>
2017-10-03 13:35:05 -0400

    Successfully created color thingy

--- IterationExamples/Iteration Examples.playground/Pages/Iteration.xcplaygroundpage/Contents.swift
+++ IterationExamples/Iteration Examples.playground/Pages/Iteration.xcplaygroundpage/Contents.swift
@@ -1,42 +1,18 @@
 //: [Previous](@previous) / [Next](@next)
-//: # Iteration
-//: The following two statements are required to make the playground run. Please do not remove.
+
 import Cocoa
 import PlaygroundSupport
 
-/*:
- ### Skipping values in a range
- The *stride* function can be combined with a *for-in* loop to move through a range of numbers, while also skipping some values.
- 
- This is a *very powerful* concept *(hint, hint)*.
- 
- For example, here how to use this type of loop to create a ladder image.
- */
 // Create canvas
 let canvas = Canvas(width: 300, height: 400)
 
 canvas.drawShapesWithBorders = false
 
-// Draw the "rungs" of a ladder
-for y in stride(from: 0, through: 400, by: 50) {
-    
-    y
-    
-    // Draw each rung
-    canvas.drawLine(fromX: 100, fromY: y, toX: 200, toY: y)
-    
-    canvas
-    
-}
-/*:
- ### Counting backwards
- You can use a *for-in* loop with the *stride* function to count backwards as well.
- */
-// Countdown, then blast off
-for value in stride(from: 10, through: 0, by: -1) {
-    print(value)
-}
-print("Blast off!")
+for i in stride(from: 0, to: 500, by: 50)
+    {
+        canvas.fillColor = Color(hue: 0, saturation: 100, brightness: i/5, alpha: 100)
+        canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: i, width: canvas.width, height: 100)
+    }
 //: **NOTE:** Output will show up in the Debug area.
 //:
 //: If you don't see this, press **Command-Shift-Y**.
